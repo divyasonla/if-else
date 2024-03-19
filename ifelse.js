9e167c4c398bbcabbe1dc071812cd542e846bb4c
divyasonla <163973310+divyasonla@users.noreply.github.com>
2024-03-19 11:19:35 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,263 @@
+// Write a program to take 4 numbers from the user and output the third max of these 4 numbers.
+
+
+// const input = require("readline-sync")
+//  let a =input.questionInt("enter a number:");
+//  let b =input.questionInt("enter a number:");
+//  let c =input.questionInt("enter a number:");
+//  let d =input.questionInt("enter a number:");
+
+
+// if(a>b){
+//     max=a;min=b;
+// }
+// else{max=b;min=a;}
+// if(c>d){
+//     max1=c; min1=d;
+// }
+// else
+// {max1=d; min1=c;}
+// if
+// (max>max1)
+// {max2=max; min2=max1;}
+// else
+// {max2=max1; min2=max;
+// }
+// if(min<min1){
+//     max3=min; min3=min1;
+// }
+// else{max3=min1; min3=min;}
+// if(min2<min3)
+// {
+//    (console.log(min2))
+// }
+// else{(console.log(min3))}
+
+
+// Write a program to take three numbers from the user and print the greater number of the three numbers. (Assume all three numbers are distinct)
+
+
+// // question1
+// const input=require("readline-sync");
+// let a =input.questionInt("enter a number:");
+// let b =input.questionInt("enter a number:");
+// let c =input.questionInt("enter a number:");
+// let max;
+// const input=require("readline-sync");
+//  let min;
+// if(a>b)
+// {
+//   max=a;
+//   min=b;
+// }
+// else {
+//     max=b;
+//     min=a;
+// }
+// if(max>c)
+// {
+//     console.log("max")
+// }
+// else
+// {
+//     console.log("c")
+// }
+
+// question2 Write a program to take four numbers from the user and print the greater number of the four numbers. (assume all the numbers are distinct).
+
+
+
+// const input=require("readline-sync");
+// let a =input.questionInt("enter a number:");
+// let b =input.questionInt("enter a number:");
+// let c =input.questionInt("enter a number:");
+// let d =input.questionInt("enter a number:");
+// let max=b;
+// let min=a;
+// let max1=c;
+// let min1=d;
+// if(a>b){
+//     max=a;
+//     min=b;
+// }else
+// {
+//     max=b;
+//     min=a;
+// }
+// if(c>d){
+//     max1=c;
+//     min1=d;
+// }
+// else{
+//     max1=d;
+//     min1=c;
+// }
+// if(max>max1)
+// {
+//     console.log("max:",max)
+// }
+// else{
+//     console.log("max1:",max1)
+// }
+
+// 3no. in 2nd  max Write a program to take 3 numbers from the user and output the second max of 3 numbers.
+// (First, do it in the normal way then do it by using 3 comparisons)
+
+// const input=require("readline-sync")
+// let a=input .questionInt("entre a number")
+// let b=input .questionInt("entre a number")
+// let c=input .questionInt("entre a number")
+// if(a>b){
+//     max=a;min=b;
+// }
+// else{max=b;min=a;}
+// if(max>c)
+// {max1=max;min1=c;}
+// else
+// {max1=c;min1=max;}
+// if(min>min1)
+// {console.log(min)}
+// else
+// {console.log(min1)}
+
+// Write a program to take a year from the user and output whether a given year is a leap year or not.
+
+
+// leap year
+// const input=require("readline-sync")
+// let a =input.questionInt("enter a number")
+// if(a%4==0){
+//  if(a%100!=0)
+//  {console.log("leapyear")}
+//  else
+//  {if(a%400==0)
+//     {console.log("leapyear")
+// }
+// else{
+//     console.log("notleapyear")
+// }
+// }
+// }
+
+// positive 
+// Write a program to take a number from the user and output whether that number is negative, positive or zero.
+
+
+// const input =require("readline-sync")
+// let a=input.questionInt("enter a number")
+// if(a>0){if(a==0){console.log("zero")}
+// else(console.log("positive"))
+// }
+// else if(a<0){console.log("negative")}
+// // else{console.log("positiv")}
+
+// odd or evenWrite a program to take a number from the user and print that number as Odd or Even.
+
+
+// const input=require("readline-sync")
+// let a =input.questionInt("enter a number")
+// if(a%2==0){console.log("even")}
+// else{console.log("odd")}
+
+// greater No.Write a program to take two numbers from the user and determine the greater of those two given numbers.
+
+
+
+// const input =require("readline-sync")
+// let a=input.questionInt("enter a number")
+// let b=input.questionInt("enter a number")
+// if(a>b){console.log("a")}
+// else{console.log("b")}
+
+
+// Write a program to take the basic salary of an employee and calculate its Gross salary according to the following: (Gross salary is the sum of basic salary, HRA, and DRA)
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
+
+
+
+// const input=require("readline-sync")
+// let bs=input.questionInt("enter a number")
+// if(bs<=1000)
+// {hra=bs*20/100;
+//  da=n*80/100;
+//   gs=bs+hra+da;
+// }
+//  else 
+//  {if(bs>20000)
+//  {hra=bs*30/100;
+// da=bs*90/100;
+// gs=bs+hra+da;}
+// else{hra=bs*25/100;
+// da=bs*90/100;
+// gs=bs+hra+da;}
+// }
+// console.log(gs)
+
+
+// const input=require("readline-sync")
+// let a=input.questionInt("enter a number")
+// let b=input.questionInt("enter a number")
+// let c=input.questionInt("enter a number")
+// let d=input.questionInt("enter a number")
+// let e=input.questionInt("enter a number")
+// let f=input.questionInt("enter a number")
+// let g=input.questionInt("enter a number")
+// let h=input.questionInt("enter a number")
+// let max;
+// let min;
+// let max1;
+// let min1;
+// let max2;
+// let min2;
+// let max3;
+// let min3;
+// let max4;
+// let min4;
+// let max5;
+// let min5;
+// let max6;
+// let min6;
+// let max7;
+// let min7;
+// let max8;
+// let min8;
+// let max9;
+// let min9;
+// let max10;
+// let min10;
+// if(a>b){max=a;min=b;}
+// else{max=b;min=a;}
+// if(c>d){max1=c;min1=d;}
+// else{max1=d;min1=c;}
+// if(e>f){max2=e;min2=f;}
+// else{max2=f;max2=e;}
+// if(g>h){max3=g;min3=h;}
+// else{max3=h;min3=f;}
+// if(max>max1){max4=max;min4=max1;}
+// else{max4=max1;min4=max;}
+// if(max2>max3){max5=max2;min5=max3;}
+// else{max5=max3;min5=max2;}
+// if(min>min1){max6=min;min6=min1;}
+// else{max6=min1;min6=min}
+// if(min2>min3){max7=min2;min7=min3;}
+// else{max7=min3;min7=min2;}
+// if(max4>max5){max8=max4;min8=max5;}
+// else{max8=max5;min8=max4;}
+// if(max6>max7){max9=max6;min9=max7;}
+// else{max9=max5;min9=max6;}
+// if(max7>max8){console.log(max7)}
+// else{console.log(max8)}
+
+
+// Write a program to take an integer as input and print the smallest positive integer that is a multiple of both 2 and n.
+
+
+/// small positive No .
+// const input=require("readline-sync")
+// let x=input.questionInt("enter a number");
+// if(x%2==0)
+// {console.log(x)}
+// // else{x=x*2;
+// console.log(x)}
